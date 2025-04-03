d00ec6bddd04087d12f6f7baa9a35fdc95d78ff5
kiu_sirdadze.giorgi <sirdadze.giorgi@kiu.edu.ge>
2025-04-03 21:21:28 +0400

    NavigationBar v1

--- app/src/main/java/com/example/findit/presentation/common/MainActivity.kt
+++ app/src/main/java/com/example/findit/presentation/common/MainActivity.kt
@@ -4,18 +4,53 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.findit.R
+import com.example.findit.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavView.setupWithNavController(navController)
+
+
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.nav_addPost -> {
+                    navController.navigate(R.id.addPostFragment)
+                    true
+                }
+
+                R.id.nav_location -> {
+                    navController.navigate(R.id.locationFragment)
+                    true
+                }
+                R.id.nav_profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
 }
